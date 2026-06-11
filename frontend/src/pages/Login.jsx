ffddffa4d6d00aeee5d94982589fc2a1a101c8cc
Braxon2 <dusanb80@gmail.com>
2026-06-11 18:08:57 +0200

    Login page changed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <form className="login-form" onSubmit={handleSubmit}>
-        <h2>Log In 1214321</h2>
+        <h2>Log In</h2>
 
         <label>Email</label>
         <input
